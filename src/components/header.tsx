eab9f450e537dce4f09611ee28dcb58d3f3c8451
Naina Johri <johrinaina80@gmail.com>
2025-03-11 23:28:17 +0530

    changes made in navbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,6 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@radix-ui/react-avatar'
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu'
-import { Link2, LinkIcon, LogOut } from 'lucide-react'
+import {LinkIcon, LogOut } from 'lucide-react'
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { Button } from './ui/button'
@@ -22,18 +22,19 @@ const Header: React.FC = () => {
                 <Button onClick={() => navigate("/auth")}> Login</Button>    
                 :(
                   <DropdownMenu>
-                  <DropdownMenuTrigger>
+                  <DropdownMenuTrigger className='w-10 rounded-full overflow-hidden '>
                   <Avatar >
-                    <AvatarImage className='rounded-full h-12 mr-1' src="https://github.com/shadcn.png" />
+                    <AvatarImage src="https://github.com/shadcn.png" />
                     <AvatarFallback>NJ</AvatarFallback>
                   </Avatar>
 
                   </DropdownMenuTrigger>
                   <DropdownMenuContent>
-                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuLabel>Naina Johri</DropdownMenuLabel>
                     <DropdownMenuSeparator />
-                    <DropdownMenuItem className='flex items-center'> <LinkIcon className='mr-2 h-4 w-4'/> My Links</DropdownMenuItem>
-                    <DropdownMenuItem className='flex items-center'> <LogOut  className='mr-2 h-4 w-4 text-red-500'/>LogOut</DropdownMenuItem>
+                    <DropdownMenuItem className='flex items-center outline-none cursor-pointer '> <LinkIcon className='mr-2 h-4 w-4'/> My Links</DropdownMenuItem>
+                    <DropdownMenuItem className='flex items-center outline-none cursor-pointer'> <LogOut  className='mr-2 h-4 w-4 text-red-500'/>
+                    <span>LogOut</span> </DropdownMenuItem>
                   </DropdownMenuContent>
                 </DropdownMenu>
                 )
