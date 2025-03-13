eb82686314364c080ad0c93fc42e1b0d4d2a332f
Naina Johri <johrinaina80@gmail.com>
2025-03-14 00:42:11 +0530

    login/signup feature is added using email

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+
 
 const Auth = () => {
+  const[searchParams] = useSearchParams();
   return (
-    <div>Auth</div>
+    <div className='flex flex-col items-center gap-10 mt-20'>
+      <h1 className='text-5xl font-extrabold'>
+        {searchParams.get("createNew")? "Hold Up! Let's login first..": "Login/ Signup"}
+      </h1>
+      <Tabs defaultValue="Login" className="w-[400px]">
+  <TabsList className=' w-full flex'>
+    <TabsTrigger value="Login">Login</TabsTrigger>
+    <TabsTrigger value="Signup">Signup</TabsTrigger>
+  </TabsList>
+  <TabsContent value="Login">Login Component </TabsContent>
+  <TabsContent value="Signup">Signup Component</TabsContent>
+</Tabs>
+
+    </div>
   )
 }
 
