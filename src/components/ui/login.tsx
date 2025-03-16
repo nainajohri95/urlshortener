4b547632e24c525f7c983ba98a635ad820c92b31
Naina Johri <johrinaina80@gmail.com>
2025-03-17 00:05:47 +0530

    Login and Signup component created

--- src/components/ui/login.tsx
+++ src/components/ui/login.tsx
@@ -1,8 +1,39 @@
 import React from 'react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from './input'
+import { Button } from './button'
+import BeatLoader from "react-spinners/BeatLoader";
+
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <Card>
+  <CardHeader>
+    <CardTitle>Login</CardTitle>
+    <CardDescription>to your account if you already have one</CardDescription>
+  </CardHeader>
+  <CardContent className='space-y-2'>
+    <div className='space-y-1'>
+      <Input name='email' type='email' placeholder='Enter Email'></Input>
+    </div>
+  </CardContent>
+  <CardContent className='space-y-2'>
+    <div className='space-y-1'>
+      <Input name='password' type='password' placeholder='Enter Password'></Input>
+    </div>
+  </CardContent>
+  <CardFooter>
+    <Button>{true? <BeatLoader size={10} color='#36d7b7'/>: "Login"}</Button>
+  </CardFooter>
+</Card>
+
   )
 }
 
---
+++ src/db/apiAuth.tsx
@@ -0,0 +1,7 @@
+import supabase from "./suprebase";
+ export  async function login({email,password}) {
+   const{data, error} =  await supabase.auth.signInWithPassword({email,password});
+
+   if(error) throw new Error(error.message);
+   return data;
+ } 
