bf9845ea2662152af453c569fcb16dd4e10a549b
Naina Johri <johrinaina80@gmail.com>
2025-03-19 23:42:09 +0530

    changes made

--- src/db/apiAuth.tsx
+++ src/db/apiAuth.tsx
@@ -1,7 +1,8 @@
 import supabase from "./suprebase";
- export  async function login({email,password}) {
-   const{data, error} =  await supabase.auth.signInWithPassword({email,password});
 
-   if(error) throw new Error(error.message);
-   return data;
- } 
+export async function login({email, password}) {
+  const {data, error} = await supabase.auth.signInWithPassword({email, password});
+  
+  if(error) throw new Error(error.message);
+  return data;
+}
